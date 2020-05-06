9fe2351e6ad8542a253f1bee967823a128ce4737
Argus-Two <argus.two.2@gmail.com>
2020-05-06 00:21:49 -0700

    added changelog

--- lib/solar_verde/cli.rb
+++ lib/solar_verde/cli.rb
@@ -7,6 +7,7 @@ class SolarVerde::CLI
     puts "Welcome to the solar output estimator"
     puts "-------------------------------------"
     get_location
+    choices
   end
 
   def get_location
@@ -19,8 +20,39 @@ class SolarVerde::CLI
   end
 
   def choices
-    puts "Please select one of the options below"
+    puts "Select an option below by entering its corresponding number"
     puts "--------------------------------------"
     puts ""
-    
+    puts "1. A yearly predicted output precentage"
+    puts ""
+    puts "2. A selected monthly output percentage"
+    puts ""
+    puts "3. Exit"
+    puts ""
+
+    response = nil
+    while response != "3"
+      response = gets.strip
+      p response
+      case response
+      when "2"
+        puts "Enter a month, (i.e January, user would enter 1)"
+        puts ""
+        puts "procesing your request, one moment please."
+        return 2
+      when "1"
+        puts ""
+        puts "processing your request, one moment please."
+        puts ""
+        return 1
+      when "3"
+        puts "Come back soon."
+        return 3
+      end
+    end
+  end
+
+
+
+
 end
